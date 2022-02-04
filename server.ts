01e7ce90f8de0346bd2c184b6b59757c0ac48d5c
Jiahao Cai <cai.jiah@husky.neu.edu>
2022-02-03 22:18:46 -0500

    minor changes

--- server.ts
+++ server.ts
@@ -5,8 +5,8 @@ import TuitController from "./controllers/TuitController";
 import UserController from "./controllers/UserController";
 import UserDao from "./daos/UserDao";
 import TuitDao from "./daos/TuitDao";
-const app = express();
 
+const app = express();
 mongoose.connect('mongodb://localhost:27017/tuiter');
 
 // configure HTTP body parser
@@ -22,8 +22,8 @@ app.get('/add/:a/:b', (req, res) => {
     res.send(req.params.a + req.params.b);
 })
 
-UserController.getInstance(app);
-TuitController.getInstance(app);
+const userController = UserController.getInstance(app);
+const tuitController = TuitController.getInstance(app);
 
 const PORT = 4000;
 app.listen(process.env.PORT || PORT);
