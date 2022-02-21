1f0528f646e440d8c45856f7661fbeae1e445dfc
Jiahao Cai <cai.jiah@husky.neu.edu>
2022-02-21 15:01:02 -0500

    no duplicate likes

--- mongoose/likes/LikeSchema.ts
+++ mongoose/likes/LikeSchema.ts
@@ -14,4 +14,5 @@ const LikeSchema = new mongoose.Schema<Like>({
     likedBy: {type: Schema.Types.ObjectId, ref: 'UserModel'}
 }, {collection: "likes"})
 
+LikeSchema.index({tuit: 1, likedBy: 1})
 export default LikeSchema
